6870f14fa7cfc5bf1a907939f8ad2a2c4991dac1
JoJimi <duddnddnfl@naver.com>
2025-11-27 03:17:21 +0900

    feat: @EnableJpaAuditing 추가

--- inventory-service/src/main/java/org/example/inventory/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/example/inventory/InventoryServiceApplication.java
@@ -2,7 +2,9 @@ package org.example.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class InventoryServiceApplication {
 
